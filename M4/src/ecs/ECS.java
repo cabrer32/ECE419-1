af0edbef6f615df32ce27d65c35c73eb5b0710c9
Yuhui Pan <pyh601535@gmail.com>
2018-04-07 17:43:08 -0400

    connect before login

--- M4/src/ecs/ECS.java
+++ M4/src/ecs/ECS.java
@@ -15,7 +15,8 @@ import java.util.*;
 
 public class ECS {
     private static Logger logger = Logger.getRootLogger();
-    private static final String SCRIPT_TEXT = "ssh -n %s nohup java -jar /Users/wuqili/Desktop/ECE419/M4/m2-server.jar %s %s %s %s %s %s &";
+//    private static final String SCRIPT_TEXT = "ssh -n %s nohup java -jar /Users/wuqili/Desktop/ECE419/M4/m2-server.jar %s %s %s %s %s %s &";
+    private static final String SCRIPT_TEXT = "ssh -n %s nohup java -jar /Users/pannnnn/UTcourses/ECE419/Milestones/ece419/M4/m2-server.jar %s %s %s %s %s %s &";
 
     private ECSWatcher zkWatch;
 
